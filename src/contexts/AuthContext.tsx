5d323db1539d692eb88001e6af2f21f2d327ef72
João Bispo <joaobispo2077@gmail.com>
2021-09-11 12:35:33 -0300

    feat: setup axios

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,5 +1,7 @@
 import React, { createContext, useState } from 'react';
 
+import { api } from '../services/api';
+
 type SignInCredentials = {
   email: string;
   password: string;
@@ -16,8 +18,16 @@ export const AuthProvider: React.FC = ({ children }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
   async function signIn(credentials: SignInCredentials) {
-    console.log(credentials);
-    setIsAuthenticated(true);
+    try {
+      const response = await api.post('/sessions', credentials);
+      console.log('response', response.data);
+      console.log(credentials);
+      setIsAuthenticated(true);
+    } catch (err: any) {
+      console.error(err);
+      console.error(err.name);
+      console.error(err.message);
+    }
   }
 
   return (
