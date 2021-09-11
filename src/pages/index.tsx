ee6967f9e95b9da0192a4098c4b5cf689fca5c38
João Bispo <joaobispo2077@gmail.com>
2021-09-11 12:23:16 -0300

    feat: add login form

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,39 @@
 import type { NextPage } from 'next';
 
-import React from 'react';
+import React, { FormEvent, useState } from 'react';
+
+import { useAuth } from '../hooks/useAuth';
+
+import styles from './styles.module.css';
 
 const Home: NextPage = () => {
-  return <div>Hello world</div>;
+  const { signIn, isAuthenticated } = useAuth();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    console.log('isAuthenticated', isAuthenticated);
+    event.preventDefault();
+    console.log(email, password);
+    await signIn({ email, password });
+    console.log('isAuthenticated', isAuthenticated);
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className={styles.form}>
+      <input
+        type="email"
+        placeholder="e-mail"
+        onChange={(event) => setEmail(event.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="senha"
+        onChange={(event) => setPassword(event.target.value)}
+      />
+      <button type="submit">Entrar</button>
+    </form>
+  );
 };
 
 export default Home;
