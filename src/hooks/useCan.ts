1da44f6ff8d86c5adeb3c1bed95082ea2c7f0f22
João Bispo <joaobispo2077@gmail.com>
2021-10-03 10:41:34 -0300

    refactor: extract access permissions and roles validation to a function

--- src/hooks/useCan.ts
+++ src/hooks/useCan.ts
@@ -1,3 +1,5 @@
+import { validadeUserAccess } from '../utils/validadeUserAccess';
+
 import { useAuth } from './useAuth';
 
 type UseCanParams = {
@@ -12,23 +14,11 @@ export function useCan({ permissions = [], roles = [] }: UseCanParams) {
     return false;
   }
 
-  if (permissions.length > 0) {
-    const hasAllPermissions = permissions.every((permission) =>
-      user.permissions.includes(permission),
-    );
+  const userHasValidPermissions = validadeUserAccess({
+    user,
+    permissions,
+    roles,
+  });
 
-    if (!hasAllPermissions) {
-      return false;
-    }
-  }
-
-  if (roles.length > 0) {
-    const hasAllRoles = roles.some((role) => user.roles.includes(role));
-
-    if (!hasAllRoles) {
-      return false;
-    }
-  }
-
-  return true;
+  return userHasValidPermissions;
 }
